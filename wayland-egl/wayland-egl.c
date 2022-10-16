85255246f87dd3b9119ad26c4a3d04bae7f3d90f
K Tact <ktact.dev@gmail.com>
2022-10-16 10:02:18 +0900

    Bind global objects

--- wayland-egl/wayland-egl.c
+++ wayland-egl/wayland-egl.c
@@ -1,11 +1,25 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include <wayland-client-protocol.h>
 #include <wayland-client.h>
 
+typedef struct GlobalObject GlobalObject;
+struct GlobalObject {
+  struct wl_compositor *compositor;
+  struct wl_shell *shell;
+};
+
 static void global_object_is_ready(void *data, struct wl_registry *registry, uint32_t id, const char *interface, uint32_t version) {
+  GlobalObject *global = (GlobalObject *)data;
+
+  if (!strcmp(interface, "wl_compositor")) {
+    global->compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+  } else if (!strcmp(interface, "wl_shell")) {
+    global->shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
+  }
 }
 
 static void global_object_has_been_removed(void *data, struct wl_registry *registry, uint32_t id) {
@@ -29,10 +43,14 @@ int main() {
 
   struct wl_registry *registry = wl_display_get_registry(display);
   assert(registry);
-  wl_registry_add_listener(registry, &registry_listener, NULL);
+
+  GlobalObject global_object = {0};
+  wl_registry_add_listener(registry, &registry_listener, &global_object);
 
   // IMPORTANT: Wait for the server to process the request
   wl_display_roundtrip(display);
+  assert(global_object.compositor);
+  assert(global_object.shell);
 
   wl_display_disconnect(display);
 
