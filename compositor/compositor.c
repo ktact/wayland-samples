42775d3b70e9803acfab49576910516913c0a303
K Tact <ktact.dev@gmail.com>
2022-10-17 21:33:03 +0900

    Add a feature to accept client

--- compositor/compositor.c
+++ compositor/compositor.c
@@ -1,10 +1,14 @@
+#include <assert.h>
 #include <stdio.h>
 #include <wayland-server.h>
 
 int main(int argc, char *argv[]) {
   struct wl_display *display = wl_display_create();
 
-  printf("Hello, Compositor!\n");
+  const char *socket_name = wl_display_add_socket_auto(display);
+  assert(socket_name);
+
+  printf("Hello, %s!\n", socket_name);
   wl_display_run(display);
 
   return 0;
